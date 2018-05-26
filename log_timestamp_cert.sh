4f5b8a45b9591f9a6b8b2675bdd71eb4c4258578
Daniele Albrizio <daniele@albrizio.it>
2018-05-26 12:29:45 +0200

    First Commit

--- log_timestamp_cert.sh
+++ log_timestamp_cert.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
-set -x
+#set -x
+#    Copyright (C) 2017 Daniele Albrizio <daniele@albrizio.it>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 2.1 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# This is a POC for certified timestamping of freeRADIUS logs
+
 yesterday=`date -d yesterday '+%Y%m%d'`
 hashesfilename=loghashes.$yesterday.sha1
 logprefix=/var/log/radius/radacct
+
+
+#---------------------------------
 
 cat /dev/null > $logprefix/$hashesfilename
 
